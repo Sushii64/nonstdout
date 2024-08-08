c491252698aee77257a0f34fb9a2db9284b133bc
Katie <76448871+Sushii64@users.noreply.github.com>
2024-08-07 22:25:49 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nonstdout',
-    version='1.0.0',
+    version='1.0.1',
     author='Sushii64',
     packages=find_packages(),
     description='A Python library for making printing cooler with colours and loading bars.',
